b0964a4700c798074cb42217dc99593e83d174be
Camiloruas <miloruas@gmail.com>
2025-11-19 22:59:17 -0300

    integração com WHATSAPP

--- bot-service/src/index.ts
+++ bot-service/src/index.ts
@@ -1,6 +1,1 @@
-
-
-client.on('ready', async () => {
-    // **IMPORTANTE**: SUBSTITUA com as credenciais de um profissional VÁLIDO no seu BD
-    await loginProfissional('SEU_EMAIL_DE_PROFISSIONAL_AQUI', 'SUA_SENHA_AQUI'); 
-});
+import './whatsappIntegration';
